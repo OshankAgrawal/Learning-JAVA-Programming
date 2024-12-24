6e9507415bcaa58a272e17a3e998be8378e46d0d
OshankAgrawal <oshankagrawal@gmail.com>
2024-12-24 23:34:09 +0530

    Infinite Loop of JAVA

---
+++ infiniteLoop.java
@@ -0,0 +1,7 @@
+public class infiniteLoop {
+    public static void main(String[] args) {
+        for(;;){ //It is an infinite loop
+            System.out.println("Hello everyone!..:)");
+        }
+    }
+}
