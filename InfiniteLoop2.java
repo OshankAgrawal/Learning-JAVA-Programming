d235f29b9647306a4ce9210aea2f717044dcd2d9
OshankAgrawal <oshankagrawal@gmail.com>
2024-12-26 01:32:31 +0530

    Infinite do-while Loop

---
+++ InfiniteLoop2.java
@@ -0,0 +1,7 @@
+public class InfiniteLoop2 {
+    public static void main(String ags []){
+        do{
+            System.out.println("Hello everyone i am Oshank Agrawal");
+        } while(true); // Infinite Loop
+    }
+}
